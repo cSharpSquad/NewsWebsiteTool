6a8aab0b8f56bcc8884a4572a5c5911b4ed27c4e
vetelkin.aleksey <vetelkin.aleksey@student.ehu.lt>
2023-11-25 20:03:29 +0200

    author unique validate

--- NewsModels/Author.cs
+++ NewsModels/Author.cs
@@ -5,6 +5,9 @@ namespace NewsWebsite.Models
 {
     public class Author
     {
+        readonly HashSet<string> uniqueNames = new();
+        private string name;
+
         [Key]
         public long Id { get; set; }
 
@@ -13,7 +16,18 @@ namespace NewsWebsite.Models
         /// </summary>
         [Required]
         [StringLength(15, MinimumLength = 3)]
-        public string Name { get; set; }
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (!uniqueNames.Add(value))
+                {
+                    throw new InvalidOperationException($"Имя '{value}' уже существует и не может быть добавлено.");
+                }
+                name = value;
+            }
+        }
 
         //public ICollection<News> News { get; set; }
     }
