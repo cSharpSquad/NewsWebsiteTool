0ef0b839c9be0624f9582a111225ce433b23a07d
vetelkin.aleksey <vetelkin.aleksey@student.ehu.lt>
2023-11-25 20:04:47 +0200

    news unique validate

--- NewsModels/News.cs
+++ NewsModels/News.cs
@@ -1,17 +1,31 @@
-using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
 
-namespace NewsWebsite.Models
+namespace NewsModels
 {
     public class News
     {
+        readonly HashSet<string> uniqueNames = new();
+        private string title;
+
         [Key]
         public long Id { get; set; }
 
         [Required]
         [StringLength(30, MinimumLength = 5)]
-        public string Title { get; set; }
+        public string Title
+        {
+            get => title;
+            set
+            {
+                if (!uniqueNames.Add(value))
+                {
+                    throw new InvalidOperationException($"Имя '{value}' уже существует и не может быть добавлено.");
+                }
+                title = value;
+            }
+        }
 
         [Required]
         [StringLength(255, MinimumLength = 5)]
