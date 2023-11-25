e7074cfcccf217544d78275ae16cad70ceb37c19
vetelkin.aleksey <vetelkin.aleksey@student.ehu.lt>
2023-11-25 20:04:16 +0200

    Tag unique validate

--- NewsModels/Tag.cs
+++ NewsModels/Tag.cs
@@ -5,12 +5,30 @@ namespace NewsWebsite.Models
 {
     public class Tag
     {
+        readonly HashSet<string> uniqueNames = new();
+        private string name;
+
         [Key]
         public long Id { get; set; }
 
+        /// <summary>
+        /// Tag name
+        /// </summary>
         [Required]
         [StringLength(15, MinimumLength = 3)]
-        public string Name { get; set; }
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (!uniqueNames.Add(value))
+                {
+                    throw new InvalidOperationException($"Имя '{value}' уже существует и не может быть добавлено.");
+                }
+                name = value;
+            }
+        }
+
 
         //public ICollection<NewsTag> NewsTags { get; set; }
     }
