2a15057be6ad5c3433286fee7ad0c4662e842a9f
vetelkin.aleksey <vetelkin.aleksey@student.ehu.lt>
2023-11-25 18:31:20 +0200

    changed folder to class library

---
+++ NewsModels/Comment.cs
@@ -0,0 +1,23 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace NewsWebsite.Models
+{
+    public class Comment
+    {
+        [Key]
+        public long Id { get; set; }
+
+        [Required]
+        [StringLength(255, MinimumLength = 3)]
+        public string Content { get; set; }
+
+        public long NewsId { get; set; }
+
+        //public News News { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public DateTime? Modified { get; set; }
+    }
+}
